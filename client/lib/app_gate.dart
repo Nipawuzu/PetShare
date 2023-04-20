449d3febaf059ab163f7c6b950fb733fb11e8da7
dejniel01 <wujkowskidaniel01@gmail.com>
2023-04-20 10:59:36 +0200

    Add auth with temporary views

--- client/lib/app_gate.dart
+++ client/lib/app_gate.dart
@@ -1,10 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:pet_share/all_views.dart';
 import 'package:pet_share/announcements/service.dart';
 import 'package:pet_share/applications/service.dart';
 import 'package:pet_share/environment.dart';
 import 'package:provider/provider.dart';
+
+import 'package:pet_share/auth/temporary_auth_gate.dart';
 
 class AppMainGate extends StatelessWidget {
   const AppMainGate({super.key});
@@ -16,9 +17,12 @@ class AppMainGate extends StatelessWidget {
         Provider(
             create: (context) =>
                 AnnouncementService(Dio(), Environment.announcementsApiUrl)),
-        Provider(create: (context) => ApplicationService()),
+        Provider(
+          create: (context) => ApplicationService(),
+        ),
       ],
-      child: const AllViews(),
+      // child: const AllViews(),
+      child: const TemporaryAuthGate(),
     );
   }
 }
