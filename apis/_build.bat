a1398ebf0a5a0dcc0408591fc91629e876605f0a
pavonism <spawlonka@outlook.com>
2023-05-16 23:29:22 +0200

    Fix _run script for apis

--- apis/_build.bat
+++
@@ -1,9 +0,0 @@
-set APIS=AdopterAPI AnnouncementsAPI ShelterAPI
-set NET_VERSION=net7.0
-
-(for %%a in (%APIS%) do ( 
-   echo ===============================================
-   echo Building %%a
-   echo ===============================================
-   dotnet build %%a
-))
