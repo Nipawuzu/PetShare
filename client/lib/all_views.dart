23748a26ce6c512c20b42e7250f0ea55b12eb77a
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-17 21:52:26 +0200

    Add observed announcements views

--- client/lib/all_views.dart
+++ client/lib/all_views.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pet_share/announcements/added_announcements/announcement_tiles_grid.dart';
+import 'package:pet_share/announcements/added_announcements/added_anouncements.dart';
 import 'package:pet_share/announcements/form/view.dart';
+import 'package:pet_share/announcements/observed_announcements/observed_announcements_grid.dart';
 import 'package:pet_share/applications/received_applications/view.dart';
 import 'package:pet_share/shelter/main_screen/view.dart';
 
@@ -20,7 +21,7 @@ class AllViews extends StatelessWidget {
           ),
           ViewsListTile(
             text: "Dodane ogłoszenia",
-            child: AnnouncementsGrid(
+            child: AddedAnnouncements(
               announcementService: context.read(),
               adopterService: context.read(),
             ),
@@ -33,6 +34,13 @@ class AllViews extends StatelessWidget {
             text: "Widok sheltera",
             child: ShelterMainScreen(),
           ),
+          ViewsListTile(
+            text: "Obserwowane\nogłoszenia",
+            child: ObservedAnnouncements(
+              announcementService: context.read(),
+              adopterService: context.read(),
+            ),
+          )
         ],
       ),
     );
