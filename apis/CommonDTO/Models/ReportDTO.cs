762b4bc41ae842379368f94a58d80313e5c08141
dejniel01 <wujkowskidaniel01@gmail.com>
2023-05-28 19:11:02 +0200

    Add reports API

---
+++ apis/CommonDTO/Models/ReportDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace CommonDTOLibrary.Models
+{
+    public class ReportDTO
+    {
+        public Guid Id { get; set; }
+        public Guid TargetId { get; set; }
+        public ReportTypeDTO ReportType { get; set; }
+        public string? Message { get; set; }
+        public ReportStateDTO State { get; set; }
+    }
+
+    public enum ReportTypeDTO
+    {
+        Adopter,
+        Announcement,
+        Shelter
+    }
+
+    public enum ReportStateDTO
+    {
+        New,
+        Accepted,
+        Declined
+    }
+}
---
+++ apis/ReportsAPI/ModelMapper.cs
@@ -0,0 +1,20 @@
+﻿using CommonDTOLibrary.Mappers;
+using DatabaseContextLibrary.models;
+using ReportsAPI.Requests;
+
+namespace ReportsAPI
+{
+    public static class ModelMapper
+    {
+        public static Report Map(this PostReportRequest reportRequest)
+        {
+            return new Report()
+            {
+                Message = reportRequest.Message,
+                ReportType = reportRequest.ReportType.MapDTO<ReportType>(),
+                TargetId = reportRequest.TargetId,
+                State = ReportState.New
+            };
+        }
+    }
+}
