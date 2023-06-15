87e3856dfb02be3298ed32c97c99554c2a8d8bfa
Dejniel01 <58104041+Dejniel01@users.noreply.github.com>
2023-06-15 13:49:54 +0200

    Fix secrets
    * Add singular secrets to set up api secrets
    
    * Add single secrets to deploy wokflow
    
    * Add single secrets to test apis
    
    * Change getting secrets in storage

--- apis/FileStorageService/GoogleFileStorage.cs
+++ apis/FileStorageService/GoogleFileStorage.cs
@@ -2,6 +2,7 @@
 using Google.Cloud.Storage.V1;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
+using System.Text;
 
 namespace FileStorageLibrary
 {
@@ -24,7 +25,7 @@ namespace FileStorageLibrary
 
         public GoogleFileStorage()
         {
-            var secretJson = _config[_secretKeyName];
+            var secretJson = GetSecretJson();
             var credentials = GoogleCredential.FromJson(secretJson);
             _client = StorageClient.Create(credentials);
         }
@@ -76,6 +77,24 @@ namespace FileStorageLibrary
         public static string GetDownloadUrl(string filename)
         {
             return $"{_hostUrl}/{_bucketName}/{filename}";
+        }
+
+        private static string GetSecretJson()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine($"\t\"type\": \"{_config[$"{_secretKeyName}:type"]}\",");
+            sb.AppendLine($"\t\"project_id\": \"{_config[$"{_secretKeyName}:project_id"]}\",");
+            sb.AppendLine($"\t\"private_key_id\": \"{_config[$"{_secretKeyName}:private_key_id"]}\",");
+            sb.AppendLine($"\t\"private_key\": \"{_config[$"{_secretKeyName}:private_key"]}\",");
+            sb.AppendLine($"\t\"client_email\": \"{_config[$"{_secretKeyName}:client_email"]}\",");
+            sb.AppendLine($"\t\"client_id\": \"{_config[$"{_secretKeyName}:client_id"]}\",");
+            sb.AppendLine($"\t\"auth_uri\": \"{_config[$"{_secretKeyName}:auth_uri"]}\",");
+            sb.AppendLine($"\t\"token_uri\": \"{_config[$"{_secretKeyName}:token_uri"]}\",");
+            sb.AppendLine($"\t\"auth_provider_x509_cert_url\": \"{_config[$"{_secretKeyName}:auth_provider_x509_cert_url"]}\",");
+            sb.AppendLine($"\t\"client_x509_cert_url\": \"{_config[$"{_secretKeyName}:client_x509_cert_url"]}\"");
+            sb.AppendLine("}");
+            return sb.ToString();
         }
     }
 }
